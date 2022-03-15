c76a0033b76d3201ac64a54700e3d28da120b5fd
denu <denu@adobe.com>
2022-03-15 13:59:11 -0400

    Add JUnit tests

--- test/TestClass.java
+++ test/TestClass.java
@@ -1,8 +1,10 @@
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class TestClass {
   @Test
-  public void testFitsSquareHole() throws Exception {
+  public void testFitsSquareHole() {
     System.out.println("Yo.");
   }
 }
